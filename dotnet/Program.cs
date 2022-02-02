07d8d52ae39aaa4b65d935ca5944eaea79695633
Kasper <kw2011@live.dk>
2022-02-02 10:23:10 +0100

    dotnet

---
+++ dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace dotnet
+{
+    class Program
+    {
+        private static string readme = File.ReadAllText("../README.md");
+        static void Main(string[] args)
+        {
+            Console.WriteLine(readme);
+        }
+    }
+}
